5e30b711c55bbdaf21aa8a83f545d6654f9690aa
Marjan Nikolic <marjan1505@gmail.com>
2016-01-28 23:00:02 +0100

    Hi, PHP

---
+++ welcome.php
@@ -0,0 +1,9 @@
+<?php
+// My name
+$name = "My name is Marjan Nikolic.";
+// This is my ocupation
+$my_ocupation = "I'm a web developer and music teacher";
+
+// Here, I show something about me
+echo $name . '</br>' . $my_ocupation;
+?>
